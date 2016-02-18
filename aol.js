a96f7b41897d2ca4b76effa9fc2ce03871b6111b
Kit Westneat <kit@pbh-network.com>
2016-02-18 12:19:23 -0500

    add AOL adapter

---
+++ aol.js
@@ -0,0 +1,28 @@
+var bid_url = 'https://adserver.adtechus.com/pubapi/3.0/';
+
+module.exports = {
+	bid: function(breq, full_breq, bid_state) {
+		var urls = [];
+		bid_state.url_to_impid = {};
+        for (var i = 0; i < breq.imp.length; i++) {
+			// aol doesn't use any client provided IDs in their response so we need to map the request URL with the imp ID
+			var url = bid_url + breq.imp[i].tagid + '/;noperf=1;alias=;cmd=bid;cors=yes;';
+			urls.push(url);
+			bid_state.url_to_impid[url] = breq.imp[i].id;
+		}
+
+		return urls;
+	},
+	process: function(body, bid_state) {
+		var bresp = JSON.parse(body);
+
+		var bid = bresp.seatbid[0].bid[0];
+		bid.id = bid.impid;
+		bid.impid = bid_state.url_to_impid[bid_state._url];
+
+		return bresp;
+	},
+	set_config: function(config) {
+		bid_url = config.bid_url || bid_url;
+	},
+}
