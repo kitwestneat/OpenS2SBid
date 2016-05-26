4096fb694a384259951eb2e58b7cce59185828bb
Kit Westneat <kit@pbh-network.com>
2016-05-26 13:25:33 -0400

    fix up pulsepoint adapter
    remove unused tag ids and return empty object if response is empty

--- pulsepoint.js
+++ pulsepoint.js
@@ -3,14 +3,19 @@ var bid_url;
 
 module.exports = {
 	bid: function(breq, full_breq) {
-		console.log('pulsepoint');
+		breq.imp.forEach(function(i) {
+			delete i.tagid;
+		});
+		console.log("breq", breq);
 		var p = utils.http_post({ url: bid_url, json: true, body: breq });
-		console.log(p);
 		return [p];
 	},
 	process: function(body) {
-		console.log("pulespoint resp", body);
-		return JSON.parse(body);
+		console.log('pulsepoint body', JSON.stringify(body, null, 4));
+		if (!body)
+			return {};
+
+		return body;
 	},
 	set_config: function(config) {
 		bid_url = config.bid_url || bid_url;
